eadc293bc192c12a22a7d260e40c5220a6bfd85e
natsuki-engr <biafrajr@gmail.com>
2024-08-16 08:56:48 +0900

    init commnand

--- command/Init.php
+++ command/Init.php
@@ -2,19 +2,37 @@
 
 require_once dirname(__FILE__) . '/CommandBase.php';
 
-class Init extends CommandBase {
+class Init extends CommandBase
+{
     public static $command = 'init';
 
     protected static $phitDir = './.phit';
 
-    public static function execute(array $args) {
+    protected static $defaultBranch = 'ref: refs/heads/main';
+
+    public static function execute(array $args)
+    {
         echo 'init';
 
-        if(is_dir(self::$phitDir)) {
+        if (is_dir(self::$phitDir)) {
             echo self::$phitDir . ' directory already exists';
             exit(1);
         } else {
             mkdir(self::$phitDir);
+            self::initDir();
         }
+
+        echo "your phit is initialized";
+    }
+
+    protected static function initDir()
+    {
+        $headFIle = fopen(self::$phitDir . '/HEAD', 'w');
+        fwrite($headFIle, self::$defaultBranch);
+
+        mkdir(self::$phitDir . "/objects");
+
+        mkdir(self::$phitDir . "/refs");
+        mkdir(self::$phitDir . '/refs/heads');
     }
 }
