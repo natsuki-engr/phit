6c61046cd3352e79502ae63c9c81b306ab522098
natsuki ueda <biafrajr@gmail.com>
2025-03-25 23:37:51 +0900

    integrate symphony/console

---
+++ src/Command/WorldCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'world')]
+class WorldCommand extends Command
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        echo 'Hello, World!';
+
+        return Command::SUCCESS;
+    }
+}
